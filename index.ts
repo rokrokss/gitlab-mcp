d69af019bd2fc804bec95330c840a195c9f3297b
Hyungrok Kim <hyungrok.kim@nexpace.io>
2026-03-25 21:24:57 +0900

    chore: remove pino pretty log

--- index.ts
+++ index.ts
@@ -276,14 +276,6 @@ import { pino } from "pino";
 
 const logger = pino({
   level: process.env.LOG_LEVEL || "info",
-  transport: {
-    target: "pino-pretty",
-    options: {
-      colorize: true,
-      levelFirst: true,
-      destination: 2,
-    },
-  },
 });
 
 /**
